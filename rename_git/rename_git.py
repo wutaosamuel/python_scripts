232f3297ffe8a8e70d1af02ead3059e0ad926baa
wutao <497863986@qq.com>
2020-01-04 14:55:12 +0800

    changed sort_in_git & need to finish rename_git

---
+++ rename_git/rename_git.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import getopt
+
+
+class rename_git:
+
+  def exec_opt(self, argv):
+    try:
+      opts, args = getopt.getopt(argv, "hi:o:c:f:n:ps",
+        ["help", "input=", "output=", "char=", "format=", "number=", "prefix", "suffix"])
+    except getopt.GetoptError:
+      print("Error: invalid usage")
+      print()
+      self.usage()
+
+    # read argv
+    for opt, arg in opts:
+      if opt in ("-h", "--help", "help"):
+        self.print_help()
+    
+
+  def exec_git(self):
+    pass
+
+  #################### HELP ####################
+
+  def print_more_help(self):
+    print("rename_git.py -h || --help for more helps")
+
+  def usage(self):
+    print("rename_git.py [-i <inputfolder>] [option]")
+    print("or")
+    print()
+    self.print_more_help()
+
+  def print_help(self):
+    self.usage()
+    print()
+    print("-i, --input \t directory of files required to rename")
+    print()
+    print("Option:")
+    print("-c, --char \t add same char on files, combined with -p | -s")
+    print("-f, --format \t change format of files")
+    print("-h, --help \t list all the helps and exit")
+    print("-n, --number \t add integer number at end of file name, start with n")
+    print("-o, --output \t directory where renamed fires move to")
+    print("-p, --prefix \t add extra characteristic on the start of file name, default is _")
+    print("-s, --suffix \t add extra characteristic on the end of file name, default is _")
+
+    # TODO: rename all files with options
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+  pass
